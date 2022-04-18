ea244fe19cdd8a1f3580b87a4a1c02d39d56eeb4
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-04-18 13:24:10 +0530

    PR

--- color flipper/app.js
+++ color flipper/app.js
@@ -1,1 +1,14 @@
-const colors = ["green","red","rgba(133,122,200)","#f15023"];
+function changecolor(){
+    const randomNumber = getrandom();
+    console.log(randomNumber);
+     document.body.style.backgroundColor = colors[randomNumber];
+     colorc.textContent = colors[randomNumber];
+}
+function getrandom(){
+    return Math.floor(Math.random()*colors.length);
+}
+const colors = ["green","red","rgba(133,122,200)","#f15023","grey",'yellow','violet'];
+const btn = document.getElementById('btn');
+const colorc  =  document.querySelector('.color');
+ btn.addEventListener('click',changecolor);
+ 
