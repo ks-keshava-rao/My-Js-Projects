0505cd7649680e50d3b1fad37f00f244a763f596
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-04-18 15:32:29 +0530

    pr

--- color flipper/hex.js
+++ color flipper/hex.js
@@ -1,12 +1,14 @@
 function hexcolor(){
     let hexCode = "#"
     for(let i = 0 ;i<6;i++){
-    hexCode = hexCode + hex[15];
+    hexCode = hexCode + hex[getrandom()];
     }
     colorc.textContent = hexCode;
     document.body.style.backgroundColor = hexCode;
 }
-
+function getrandom(){
+    return Math.floor(Math.random()*hex.length)
+}
 const hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
 const btn = document.getElementById('btn');
 const colorc = document.querySelector('.color');
