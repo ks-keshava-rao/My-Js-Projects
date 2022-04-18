9cad6dd069212f04fe2af6bf4caef32925253be2
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-04-18 15:45:59 +0530

    pr

--- color flipper/hex.js
+++ color flipper/hex.js
@@ -11,6 +11,10 @@ function getrandom(){
 }
 const hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
 const btn = document.getElementById('btn');
+const btns = document.getElementById('btns');
 const colorc = document.querySelector('.color');
-btn.addEventListener('click',hexcolor)
-
+btn.addEventListener('click',hexcolor);
+btns.addEventListener('click',()=>{
+    document.body.style.backgroundColor = "white";
+    colorc.textContent = "Click to Change";
+})
