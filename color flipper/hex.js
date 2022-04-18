3019060322e35f09e6984f2461ea4ef1fc07ef8d
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-04-18 14:44:54 +0530

    pr

--- color flipper/hex.js
+++ color flipper/hex.js
@@ -1,3 +1,13 @@
+function hexcolor(){
+    let hexCode = "#"
+    for(let i = 0 ;i<6;i++){
+    hexCode = hexCode + hex[0];
+    }
+    colorc.textContent = hexCode;
+    document.body.style.backgroundColor = hexCode;
+}
 const hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
 const btn = document.getElementById('btn');
-const colorc = document.getElementsByClassName('color');
+const colorc = document.querySelector('.color');
+btn.addEventListener('click',hexcolor)
+
