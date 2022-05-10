b2cdea4da3700fe822f5e8a74c22b6cc05dfa63a
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-05-10 18:50:13 +0530

    pr

--- UserManagementTool/App.js
+++ UserManagementTool/App.js
@@ -6,26 +6,54 @@
 const dataInput = document.querySelectorAll(".inputs")
 const submitbtn = document.getElementById("subbtn");
 submitbtn.addEventListener('click', submitData)
+userData = {};
+dataArray = [];
 const method = {
-    displayData : function(){
-        return(`${this.ename},${this.id},${this.role}`);
+    displayData: function () {
+        return (`${this.ename},${this.id},${this.role}`);
     },
-    convertToArray: function(){
+    convertToArray: function () {
         return [this];
+    },
+
+}
+function validate(empName, empId, role) {
+    if (empName.value == null || empName.value == "") {
+        alert("Name cannot be blank");
+        return false;
     }
-} 
-function submitData(e){
-    userData = {};
-    // e.preventDefault();
+    else if (empId.value == "" || empId.value == null) {
+        alert("ID cannot be empty");
+        return false;
+    }
+    else if (role.value == "" || role.value == null) {
+        alert("role cannot be empty");
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+function submitData(e) {
+    e.preventDefault();
+   
     const empName = dataInput[0];
     const empId = dataInput[1];
     const role = dataInput[2];
-    userData.ename = empName.value;
-    userData.id = empId.value;
-    userData.role = role.value;
-    let displaydata = method.displayData.call(userData);
-    let dataArray = method.convertToArray.call(userData);
-    // console.log(dataArray);
-    
-    alert(`Details" ${displaydata}  "added succesfully`);
+    if (validate(empName, empId, role)) {
+        userData.ename = empName.value;
+        userData.id = empId.value;
+        userData.role = role.value;
+        let displaydata = method.displayData.call(userData);
+        alert(`Details ${displaydata} added succesfully`);
+        dataArray.push(userData);
+        console.log(dataArray);
+        setData();
+    }
+    else {
+        alert("data not updated")
+    }
+}
+function setData(){
+    localStorage.setItem('DataBase',JSON.stringify(dataArray));
 }
